b429050e3522068b8977ee98d70f8460eb517d9d
parthmunjal07 <parth.munjal07@gmail.com>
2025-12-12 23:38:41 +0530

    yo

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,52 @@
-import { useContext, useEffect, useState } from "react";
-import "./App.css";
-import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
-import Login from "./components/Auth/Login";
-import AdminDashboard from "./components/Dashboard/AdminDashboard";
-import { getLocalStorage } from "./utils/LocalStorage";
-import AuthProvider, { AuthContext } from "./context/AuthProvider";
+import React, { useContext, useEffect, useState } from 'react'
+import Login from './components/Auth/Login'
+import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
+import AdminDashboard from './components/Dashboard/AdminDashboard'
+import { AuthContext } from './context/AuthProvider'
 
-function App() {
+const App = () => {
+
   const [user, setUser] = useState(null)
+  const [loggedInUserData, setLoggedInUserData] = useState(null)
+  const [userData,setUserData] = useContext(AuthContext)
+
+  useEffect(()=>{
+    const loggedInUser = localStorage.getItem('loggedInUser')
+    
+    if(loggedInUser){
+      const userData = JSON.parse(loggedInUser)
+      setUser(userData.role)
+      setLoggedInUserData(userData.data)
+    }
+
+  },[])
+
+
   const handleLogin = (email, password) => {
-    if (email == "admin@me.com" && password == "123"){
-      setUser("admin")
-    } else if (email == "user@me.com" && password == 123) {
-      setUser('employee')
-    } else {
+    if (email == 'admin@me.com' && password == '123') {
+      setUser('admin')
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: 'admin' }))
+    } else if (userData) {
+      const employee = userData.find((e) => email == e.email && e.password == password)
+      if (employee) {
+        setUser('employee')
+        setLoggedInUserData(employee)
+        localStorage.setItem('loggedInUser', JSON.stringify({ role: 'employee',data:employee }))
+      }
+    }
+    else {
       alert("Invalid Credentials")
     }
   }
-  
-  const data = AuthContext(AuthProvider)
-  
+
+
 
   return (
     <>
-      {!user ? <Login handleLogin={handleLogin}/>:''}
-      {user == "admin" ? <AdminDashboard /> : <EmployeeDashboard />}
+      {!user ? <Login handleLogin={handleLogin} /> : ''}
+      {user == 'admin' ? <AdminDashboard changeUser={setUser} /> : (user == 'employee' ? <EmployeeDashboard changeUser={setUser} data={loggedInUserData} /> : null) }
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,13 +1,19 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import { getLocalStorage } from "../utils/LocalStorage";
 
 export const AuthContext = createContext();
 
 function AuthProvider({ children }) {
-    const [] = useState()
+  const [userData, setUserData] = useState(null);
+  useEffect(() => {
+    const { employees, admin } = getLocalStorage();
+
+    setUserData({ employees, admin });
+  }, []);
 
   return (
     <div>
-      <AuthContext value={"Parth"}>{children}</AuthContext>
+      <AuthContext value={[userData,setUserData]}>{children}</AuthContext>
     </div>
   );
 }
